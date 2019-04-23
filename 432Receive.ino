f3b8225a3c8f9b5a3f82ee08bc63be93ce6b7e08
Christian H <41932906+chrisss1@users.noreply.github.com>
2019-04-23 12:20:37 -0500

    Arduino Nano Receiver code
    Pot values from HC12 will be sent to both ESCs which send the speed to the motors

---
+++ 432Receive.ino
@@ -0,0 +1,49 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+#define setPin 4
+
+Servo ESC1;
+Servo ESC2;
+SoftwareSerial HC12(3, 2);              // HC-12 TX Pin, HC-12 RX Pin
+
+void setup() {
+  Serial.begin(9600);                   // Open serial port to computer
+  HC12.begin(9600);                     // Open serial port to HC12
+
+  ESC1.attach(8,1000,2000);
+  ESC1.write(0);
+  ESC1.attach(9,1000,2000);
+  ESC1.write(0);
+  pinMode(setPin, OUTPUT);
+  
+  digitalWrite(setPin, LOW);
+  delay(250);
+  HC12.write("AT+DEFAULT");
+  delay(250);
+  digitalWrite(setPin, HIGH);           // HC-12 normal, transparent mode
+}
+
+void loop() 
+{
+
+  while( HC12.available == 0) 
+  {
+
+    if (HC12.available() > 1)               //If there is data in the buffer
+    {
+
+      int input = HC12.readStringUntil(10); //reads the incoming data until a new line is detected
+      
+      input = input.toInt();                //converts string to integer
+      
+      ESC1.write(input);
+      ESC2.write(input);
+    }
+    
+  }
+
+  delay(10)                                  //Delay for reading stability
+
+
+
+}
