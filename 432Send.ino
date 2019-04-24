f99c39518090eb89d4f852737e6bb8cd83d2aa39
Christian H <41932906+chrisss1@users.noreply.github.com>
2019-04-23 19:12:28 -0500

    Update 432Send.ino

--- 432Send.ino
+++ 432Send.ino
@@ -6,11 +6,16 @@ int ledPin = 8;       // select the pin for the LED
 int buttonPin = 7;    // push button pin
 int val = 0; 
 
+Servo ESC1;
+
 SoftwareSerial HC12(3, 2);              // HC-12 TX Pin, HC-12 RX Pin
 
 void setup() {
   Serial.begin(9600);                   // Open serial port to computer
   HC12.begin(9600);                     // Open serial port to HC12
+  
+  ESC1.attach(8,1000,2000);
+  ESC1.write(0);
   
   pinMode(ledPin, OUTPUT);              // declare LED as output
   pinMode(buttonPin, INPUT);            // declare pushbutton as input
@@ -46,16 +51,18 @@ void loop()
     Serial.println(individualSpeed);
     HC12.println(individualSpeed);
     
-    delay(50);
+    ESC1.write(platformSpeed);
     
     button = digitalRead(buttonPin);
     if (val == HIGH)                        // check if the input is HIGH (button released)
     {                  
-      digitalWrite(ledPin, HIGH);           // turn LED OFF
+      digitalWrite(ledPin, LOW);            // turn LED OFF
+      HC12.println(0);
+      ESC1.write(platformSpeed);
     } 
     else 
     {
-      digitalWrite(ledPin, LOW);            // turn LED ON
+      digitalWrite(ledPin, HIGH);           // turn LED ON
     }
     
   }
