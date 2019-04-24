92e7fd5c0c2614828dc05d34b60dadbc13cdede2
Christian H <41932906+chrisss1@users.noreply.github.com>
2019-04-23 19:24:03 -0500

    Small fixes

--- 432Receive.ino
+++ 432Receive.ino
@@ -4,7 +4,7 @@
 
 Servo ESC1;
 Servo ESC2;
-SoftwareSerial HC12(3, 2);              // HC-12 TX Pin, HC-12 RX Pin
+SoftwareSerial HC12(11, 10);              // HC-12 TX Pin, HC-12 RX Pin
 
 void setup() {
   Serial.begin(9600);                   // Open serial port to computer
@@ -14,33 +14,30 @@ void setup() {
   ESC1.write(0);
   ESC1.attach(9,1000,2000);
   ESC1.write(0);
-  pinMode(setPin, OUTPUT);
+  //pinMode(setPin, OUTPUT);
   
-  digitalWrite(setPin, LOW);
-  delay(250);
-  HC12.write("AT+DEFAULT");
-  delay(250);
-  digitalWrite(setPin, HIGH);           // HC-12 normal, transparent mode
+  //digitalWrite(setPin, LOW);
+  //delay(250);
+  //HC12.write("AT+DEFAULT");
+  //delay(250);
+  //digitalWrite(setPin, HIGH);           // HC-12 normal, transparent mode
 }
 
 void loop() 
 {
 
-  while( HC12.available == 0) 
+  while( HC12.available()) 
   {
 
-    if (HC12.available() > 1)               //If there is data in the buffer
-    {
+    int input = HC12.parseInt();              //converts string to integer
 
-      int input = HC12.parseInt();              //converts string to integer
-      
-      ESC1.write(input);
-      ESC2.write(input);
-    }
+    ESC1.write(input);
+    ESC2.write(input);
+
     
   }
 
-  delay(10);                                  //Delay for reading stability
+  //delay(10);                                  //Delay for reading stability
 
 
 
