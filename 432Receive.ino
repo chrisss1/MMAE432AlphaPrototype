79b386d49ec8950938581c840c5cbf5a50d5bc6a
Christian H <41932906+chrisss1@users.noreply.github.com>
2019-04-30 12:10:52 -0500

    Comments, ESC2 fix

--- 432Receive.ino
+++ 432Receive.ino
@@ -2,39 +2,41 @@
 #include <Servo.h>
 #define setPin 4
 
-Servo ESC1;
-Servo ESC2;
-SoftwareSerial HC12(11, 10);              // HC-12 TX Pin, HC-12 RX Pin
+Servo ESC1;                                       //Initialize ESC1 as a Servo
+Servo ESC2;                                       //Initialize ESC1 as a Servo
+SoftwareSerial HC12(11, 10);                      // HC-12 TX Pin, HC-12 RX Pin
 
 void setup() {
-  Serial.begin(9600);                   // Open serial port to computer
-  HC12.begin(9600);                     // Open serial port to HC12
+  Serial.begin(9600);                             // Open serial port to computer
+  HC12.begin(9600);                               // Open serial port to HC12
 
-  ESC1.attach(8);
-  ESC1.write(0);
-  delay(100);
-  ESC1.attach(9);
-  ESC1.write(0);
-  //pinMode(setPin, OUTPUT);
+  ESC1.attach(8);                                 //Connect ESC1 to pin 8           
+  ESC1.write(0);                                  //Write a speed of 0 to ESC1
   
+  delay(100);                                     //Delay for initialization time
+  
+  ESC2.attach(9);                                 //Connect ESC2 to pin 9   
+  ESC2.write(0);                                  //Write a speed of 0 to ESC2
+  
+  //pinMode(setPin, OUTPUT);
   //digitalWrite(setPin, LOW);
   //delay(250);
   //HC12.write("AT+DEFAULT");
   //delay(250);
-  //digitalWrite(setPin, HIGH);           // HC-12 normal, transparent mode
+  //digitalWrite(setPin, HIGH);                   // HC-12 normal, transparent mode
 }
 
 void loop() 
 {
 
-  while( HC12.available()) 
+  while( HC12.available())                        //If there is data in the HC12 Serial
   {
 
-    int input = HC12.parseInt();              //converts string to integer
+    int input = HC12.parseInt();                  //converts string to integer
 
-    Serial.println(input);
-    ESC1.write(input);
-    ESC2.write(input);
+    Serial.println(input);                        //Print it on the serial monitor for torubleshooting with PC
+    ESC1.write(input);                            //Write desired speed to ESC1
+    ESC2.write(input);                            //Write desired speed to ESC2
 
     
   }
